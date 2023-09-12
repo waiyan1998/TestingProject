7af0e3bed240917683d035c6169b52396820ca14
waiyan1998 <strobbsion@gmail.com>
2023-09-12 10:29:03 +0630

    first commit

---
+++ TestingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingProject
+//
+//  Created by AungKyawPhyoe on 12/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
